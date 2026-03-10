e733e583fd8b2546b27cd9bff50a25499d489799
littleleg198602 <honza.nozicka@gmail.com>
2026-03-10 15:37:55 +0100

    Improve wire-number splitting selection for special cable rows

---
+++ rulechecker-cz/tests/test_wire_number_formatting.py
@@ -0,0 +1,69 @@
+import unittest
+
+import pandas as pd
+
+from excel_parser import parse_rc_sheet
+from rc_maps import get_rc_definition
+
+
+class TestWireNumberFormatting(unittest.TestCase):
+    def test_splits_concatenated_wire_numbers_into_new_lines(self):
+        df = pd.DataFrame([
+            {
+                "Einschätzung": "Fehler",
+                "Leitungsnummer": "4315443155",
+                "Sonderleitung": "SL-1",
+                "Leitungen": "43154 43155",
+            }
+        ])
+
+        records = parse_rc_sheet(df, 110, get_rc_definition(110))
+
+        self.assertEqual(records[0].wire_number, "43154\n43155")
+
+    def test_replaces_delimiters_with_new_lines(self):
+        df = pd.DataFrame([
+            {
+                "Einschätzung": "Fehler",
+                "Leitungsnummer": "43154,43155",
+                "Sonderleitung": "SL-1",
+                "Leitungen": "43154 43155",
+            }
+        ])
+
+        records = parse_rc_sheet(df, 110, get_rc_definition(110))
+
+        self.assertEqual(records[0].wire_number, "43154\n43155")
+
+
+    def test_prefers_multi_wire_source_when_available(self):
+        df = pd.DataFrame([
+            {
+                "Einschätzung": "Fehler",
+                "Leitungsnummer": "4316543166",
+                "Leitungen": "43165 43166",
+                "Sonderleitung": "SL-1",
+            }
+        ])
+
+        records = parse_rc_sheet(df, 110, get_rc_definition(110))
+
+        self.assertEqual(records[0].wire_number, "43165\n43166")
+
+    def test_splits_numeric_string_with_decimal_suffix(self):
+        df = pd.DataFrame([
+            {
+                "Einschätzung": "Fehler",
+                "Leitungsnummer": "4316543166.0",
+                "Sonderleitung": "SL-1",
+                "Leitungen": "43165 43166",
+            }
+        ])
+
+        records = parse_rc_sheet(df, 110, get_rc_definition(110))
+
+        self.assertEqual(records[0].wire_number, "43165\n43166")
+
+
+if __name__ == "__main__":
+    unittest.main()
